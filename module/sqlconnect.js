fe5f5397b78729eb0e4218ffa7488e7a2fa62e86
a12wang1 <a12wang@126.com>
2019-03-29 09:06:54 +0800

    add api & app

---
+++ module/sqlconnect.js
@@ -0,0 +1,15 @@
+// import Sequelize from 'sequelize';
+var Sequelize = require('sequelize');
+var seqConn = null;
+var sequelize = function(){
+    if(seqConn==null){
+        console.log('创建连接');
+        seqConn = new Sequelize('biyedesign', 'root', '123456', {
+            host: '127.0.0.1',
+            dialect: 'mysql'
+        });
+    }
+    return seqConn;
+}
+
+module.exports = sequelize;
---
+++ routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+var sequelize = require('../module/sqlconnect')();
+
+router.get('/', function(req, res, next) {
+  console.log(req)
+  // res.render('index', { title: 'Express' });
+  res.render('register')
+});
+
+router.post ('/login',function (req, res, next) {
+  let sql = 'select password from customer where `username` = :username;';
+  let password = req.body.password;
+  sequelize.query(sql,{
+    replacements: { username: req.body.username },
+    type: sequelize.QueryTypes.SELECT
+  }).then((rs) => {
+    if(rs.length!=0){
+      for (let i=0;i<rs.length;i++) {
+        if(password==rs[i].password){
+          res.send({
+            "code":0,
+            "message":"登录成功",
+          })
+          break;
+        }
+      }
+    }else{
+      res.send( {
+        "code":2,
+        "message":"用户名密码错误，登录失败"
+
+      })
+    }
+  })
+})
+
+router.post('/register',function (req,res,next) {
+  var sql='INSERT INTO customer(`username`,`age`,`password`, `tel`, `email`) VALUE (:username, :age,:password, :tel, :email);';
+  sequelize.query(sql,{
+    replacements: {
+      username: req.body.username ,
+      age : req.body.age,
+      password : req.body.password,
+      tel : req.body.tel,
+      email : req.body.email
+    },
+    type: sequelize.QueryTypes.INSERT
+  }).then((rs)=> {
+    res.send({
+      code : 0,
+      message : "OK"
+    })
+  })
+})
+
+
+module.exports = router;
